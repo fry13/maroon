dc9459c259037d224bedf9e50296ac979db7c4b0
Alexey Feklyunin <ninetailed1995@gmail.com>
2023-03-10 19:29:26 +0400

    feat: create types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+  shortDesc: string;
+  longDesc: string;
+  composition: string;
+  application: string;
+  volume: string[];
+  cost: string;
+}
