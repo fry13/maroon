e68591e1fe025c35e738682c77bdb07e373a50ba
Alexey Feklyunin <ninetailed1995@gmail.com>
2023-03-18 15:25:47 +0400

    feat: add latest phone versions for sections

--- src/components/RecentlyViewed/RecentlyViewed.tsx
+++ src/components/RecentlyViewed/RecentlyViewed.tsx
@@ -1,16 +1,24 @@
 import { Product } from "@/types";
 import styles from "./RecentlyViewed.module.scss";
 import Catalog from "../Catalog/Catalog";
+import { useEffect, useRef } from "react";
 
 type Props = {
   data: Product[];
 };
 
 const RecentlyViewed = ({ data }: Props) => {
+  let size = useRef(4);
+  useEffect(() => {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 560) size.current = 1;
+    console.log(size.current);
+  }, [size]);
+
   return (
     <article className={styles.recently_viewed}>
       <h2>Вы недавно смотрели</h2>
-      <Catalog size={4} data={data} />
+      <Catalog size={size.current} data={data} />
     </article>
   );
 };
